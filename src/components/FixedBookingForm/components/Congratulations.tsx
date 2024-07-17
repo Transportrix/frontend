fb4b708964e8fce3f7bb147a9a6e4c94f398e210
phryki512 <davidquartey512@gmail.com>
2024-07-17 15:00:00 +0000

    congratulations

--- src/components/FixedBookingForm/components/Congratulations.tsx
+++ src/components/FixedBookingForm/components/Congratulations.tsx
@@ -1,12 +1,31 @@
 import FramerWrapper from "@/components/FramerWrapper";
 import { fadeIn } from "@/lib/animations";
+import Image from 'next/image';
 
-function Congrutalations() {
+function Congratulation() {
     return ( 
-        <FramerWrapper {...fadeIn}>
-            Congrats  (Kwartekwei)
+        <FramerWrapper {...fadeIn} className="flex flex-col min-h-screen">
+            <div className="flex flex-1 justify-center items-center space-x-4 p-8">
+                {/* Left Container with Text */}
+                <div className="flex-1 bg-white p-8 rounded-lg shadow-lg">
+                    <h2 className="text-3xl font-bold mb-4">Congratulations!</h2>
+                    <p>Your achievement is remarkable. Celebrate this moment!</p>
+                </div>
+                {/* Right Container with Image */}
+                <div className="flex-1 bg-white p-8 rounded-lg shadow-lg flex items-center justify-center">
+                    <Image
+                        src="/https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx2psfOvbDLrO2ZGJ7sTN8MKWf0Zq5KCEWag&s" // Replace with your image path
+                        alt="Celebratory Image"
+                        width={300} // Set appropriate width
+                        height={300} // Set appropriate height
+                        className="rounded-lg"
+                    />
+                </div>
+            </div>
+            {/* Continue Button */}
+           
         </FramerWrapper>
-     );
+    );
 }
 
-export default Congrutalations;
+export default Congratulation;
--- src/components/FixedBookingForm/pagesData.tsx
+++ src/components/FixedBookingForm/pagesData.tsx
@@ -1,5 +1,5 @@
 import BusDetails from "./components/BusDetails";
-import Congrutalations from "./components/Congratulations";
+import Congratulations from "./components/Congratulations";
 import Searching from "./components/Searching";
 import SelectASeat from "./components/SelectASeat";
 import StartHere from "./components/StartHere";
@@ -12,12 +12,6 @@ export const pages: any = [
     component: <StartHere />,
     number: 1,
   },
-//   {
-//     name: "start-here",
-//     label: "start-here",
-//     component: <TellUsMore />,
-//     number: 1,
-//   },
   {
     name: "start-here",
     label: "start-here",
@@ -30,16 +24,10 @@ export const pages: any = [
     component: <BusDetails />,
     number: 1,
   },
-//   {
-//     name: "start-here",
-//     label: "start-here",
-//     component: <SelectASeat />,
-//     number: 1,
-//   },
   {
     name: "start-here",
     label: "start-here",
-    component: <Congrutalations />,
+    component: <Congratulations />,
     number: 1,
   },
 ];
